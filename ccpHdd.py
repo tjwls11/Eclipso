35c9181264ed83704736e681395e769b93a38c77
gudrhkdvps07 <gudrhkdvps07@gmail.com>
2025-10-15 00:21:56 +0900

    ccpHdd  확인

--- ccpHdd.py
+++ ccpHdd.py
@@ -1,14 +1,35 @@
 import struct, olefile
 
+def le16(b, off): 
+    return struct.unpack_from("<H", b, off)[0]
+
+def le32(b, off):
+    return struct.unpack_from("<I", b, off)[0]
+
+
 with olefile.OleFileIO("근희함.doc") as ole:
     word = ole.openstream("WordDocument").read()
 
-ccpHdd = struct.unpack_from("<I", word, 0x0068)[0]
-print(f"ccpHdd = {ccpHdd}")
-if ccpHdd > 0:
-    print("헤더/푸터 문서 존재")
-else:
-    print("헤더/푸터 문서 없음")
+base_len = 32
+csw = le16(word, 32)
+fibRgW_len = csw * 2
+cslw = le32(word, 32 + 2 + fibRgW_len) # fibRgW 길이 + csw길이까지 포함
+fibRgLw_off = 32 + 2 + fibRgW_len + 2
+fibRgLw_len = cslw * 4
 
-for name in ole.listdir():
-    print(name)
+fib = {
+    "base_len": base_len,
+    "csw": csw,
+    "fibRgW_len": fibRgW_len,
+    "cslw": cslw,
+    "fibRgLw_off": fibRgLw_off,
+    "fibRgLw_len": fibRgLw_len,
+}
+
+#ccpHdd 읽기
+ccpHdd = le32(word, fibRgLw_off + 0x0C)
+
+for name, value in fib.items():
+    print(f"{name} = {value}")
+
+print(f"ccpHdd: {ccpHdd}")
